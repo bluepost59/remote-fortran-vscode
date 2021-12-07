04630954fe14791b391fa1d08e2689b8503faafa
bluepost59 <kaput.kat@gmail.com>
2021-12-07 19:55:26 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# remote-fortran-vscode
+
+VSCodeのRemote-SSHを使ってSSHサーバ上でFortran開発するための設定ファイル集。
+
---
+++ test.f90
@@ -0,0 +1,5 @@
+program main
+  implicit none
+  write(6,*) "hello world"
+  write(6,*) "end"
+end program main
